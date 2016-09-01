6393fc5519a7a284f0db7b93c44da8f158e6407c
Stephen Piccolo <goldfish1434@yahoo.com>
2016-09-01 16:15:57 -0600

    Automating test accuracy check

--- BuildTests/CheckAccuracy.py
+++ BuildTests/CheckAccuracy.py
@@ -1,3 +1,33 @@
 import os, sys, glob
 
-print "Inside CheckAccuracy.py"
+description = sys.argv[1]
+classificationAlgorithmPaths = glob.glob(sys.argv[2])
+metricFilePaths glob.glob(sys.argv[3])
+
+if len(classificationAlgorithmPaths) == 0:
+    print "No classification algorithm scripts were found!"
+    exit(1)
+
+if len(metricFilePaths) == 0:
+    print "No metric files were found!"
+    exit(1)
+
+for metricFilePath in metricFilePaths:
+    print "Checking metric file: %s" % metricFilePath
+
+    metricData = [line.rstrip().split("\t") for line in file(metricFilePath)]
+    headerItems = metricData.pop(0)
+    metricNameIndex = headerItems.index("Metric")
+    valueIndex = headerItems.index("Value")
+
+    aucValues = [float(row[valueIndex]) for row in metricData if row[metricNameIndex] == "AUROC"]
+    meanAUC = sum(aucValues) / float(len(aucValues))
+
+    if "Strong" in description:
+        threshold = 0.9
+        if meanAUC >= threshold:
+            print "[PASSED]"
+        else:
+            print "The mean AUROC was %.3f for %s. The expected lower threshold is %.3f." % (meanAUC, description, threshold)
+            print "[FAILED]"
+            exit(1)
