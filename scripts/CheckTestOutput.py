c93d6cc44fb7c0dd4a7b93cfed02cf89d84dc286
Stephen Piccolo <goldfish1434@yahoo.com>
2016-09-07 14:39:16 -0600

    Dynamically checking where we are executing build tests

--- scripts/CheckTestOutput.py
+++
@@ -1,9 +0,0 @@
-import os, sys
-
-logFilePath = sys.argv[1]
-
-logInfo = "".join([line for line in file(logFilePath)])
-
-if "[FAILED]" in logInfo:
-    print "Failures occurred, so halting."
-    sys.exit(1)
