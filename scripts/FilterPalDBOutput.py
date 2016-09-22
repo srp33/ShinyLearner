9c4ff2981bd2e00d7241a6d862238b996a650e7c
Stephen Piccolo <goldfish1434@yahoo.com>
2016-09-22 15:42:44 -0600

    Reducing noise in output

--- scripts/FilterPalDBOutput.py
+++ scripts/FilterPalDBOutput.py
@@ -2,5 +2,5 @@ import sys
 
 # This seems to be a somewhat reasonable way to avoid printing warnings.
 for line in sys.stdin:
-    if line.startswith("INFO:") or "com.linkedin.paldb.impl" in line or line.startswith("Max offset length:") or line.startswith("Slot size:") or line.startswith("Key count:") or line.startswith("Index size:") or line.startswith("Data size:") or line.startswith("Created at:") or line.startswith("Format version:") or line.startswith("Number of memory mapped data buffers:":
+    if line.startswith("INFO:") or "com.linkedin.paldb.impl" in line or line.startswith("Max offset length:") or line.startswith("Slot size:") or line.startswith("Key count:") or line.startswith("Index size:") or line.startswith("Data size:") or line.startswith("Created at:") or line.startswith("Format version:") or line.startswith("Number of memory mapped data buffers:"):
         print line.rstrip()
