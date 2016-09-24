14db3e2d4139a01d89487b70e68eb1b050204b54
Stephen Piccolo <goldfish1434@yahoo.com>
2016-09-24 15:20:08 -0600

    Debugging output to stderr and stdout

--- scripts/FilterOutput.py
+++ scripts/FilterOutput.py
@@ -11,9 +11,9 @@ else:
     for line in sys.stdin:
         line = line.strip()
 
-        if line.startswith("INFO:") or "com.linkedin.paldb.impl" in line or line.startswith("Max offset length:") or line.startswith("Slot size:") or line.startswith("Key count") or line.startswith("Index size:") or line.startswith("Data size:") or line.startswith("Created at:") or line.startswith("Format version:") or line.startswith("Number of memory mapped data buffers:") or line.startswith("Parsed with column specification") or line == "cols(" or line == ")" or "col_character()" in line or "col_integer()" in line or line.startswith("Joining, by =") or line.startswith("Number of keys:"):
+        #if line.startswith("INFO:") or "com.linkedin.paldb.impl" in line or line.startswith("Max offset length:") or line.startswith("Slot size:") or line.startswith("Key count") or line.startswith("Index size:") or line.startswith("Data size:") or line.startswith("Created at:") or line.startswith("Format version:") or line.startswith("Number of memory mapped data buffers:") or line.startswith("Parsed with column specification") or line == "cols(" or line == ")" or "col_character()" in line or "col_integer()" in line or line.startswith("Joining, by =") or line.startswith("Number of keys:"):
         #if line.startswith("INFO:"):
-            continue
+        #    continue
 
         print line
         sys.stdout.flush()
