f28b5ab8d7d46ee0bde93ddec10f4d858ab789fb
Stephen Piccolo <goldfish1434@yahoo.com>
2016-10-08 20:49:30 -0600

    Debugging build tests

--- BuildTests/CheckIdentical.py
+++ BuildTests/CheckIdentical.py
@@ -9,43 +9,20 @@ inFile2 = open(inFilePath2)
 inFile1.readline()
 inFile2.readline()
 
+misMatchCount = 0
 for line1 in inFile1:
     line2 = inFile2.readline()
     if line1 != line2:
-        print "[FAILED] Files are not identical. Printing first non-match."
-        print "File 1: %s" % inFilePath1
-        print line1.rstrip()
-        print "File 2: %s" % inFilePath2
-        print line2.rstrip()
+        print "[FAILED] Mismatch between %s and %s:" (inFilePath1, inFilePath2)
+        print "  %s" % line1.rstrip()
+        print "  %s" % line2.rstrip()
 
-        inFile2.close()
-        inFile1.close()
-        sys.exit(1)
+        misMatchCount += 1
+        if misMatchCount == 10:
+            break
 
 inFile2.close()
 inFile1.close()
 
-#if not areIdentical:
-#    print "[FAILED] Files are not identical:"
-#    print "  %s" % inFilePath1
-#    print "  %s" % inFilePath2
-#    print "  ======================================"
-#
-#
-#    inFile1 = open(inFilePath1)
-#    inFile2 = open(inFilePath2)
-#
-#    for line1 in inFile1:
-#        line2 = inFile2.readline()
-#
-#        if line1 != line2:
-#            print "  %s:" % inFilePath1
-#            print "    %s" % line1
-#            print "  %s:" % inFilePath2
-#            print "    %s" % line2
-#
-#    inFile1.close()
-#    inFile2.close()
-#    print "  ======================================"
-#
-#    sys.exit(1)
+if misMatchCount > 0:
+    sys.exit(1)
