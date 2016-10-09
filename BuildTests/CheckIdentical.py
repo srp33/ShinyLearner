8a6fccffa29888fb8343ebb951645bb1a1647e40
Stephen Piccolo <goldfish1434@yahoo.com>
2016-10-08 18:40:51 -0600

    Debugging build tests

--- BuildTests/CheckIdentical.py
+++ BuildTests/CheckIdentical.py
@@ -9,31 +9,29 @@ inFile2 = open(inFilePath2)
 inFile1.readline()
 inFile2.readline()
 
-areIdentical = True
-
 for line1 in inFile1:
     line2 = inFile2.readline()
     if line1 != line2:
-        areIdentical = False
-        break
+        print "[FAILED] Files are not identical. Printing first non-match."
+        print "File 1: %s" % inFilePath1
+        print line1.rstrip()
+        print "File 2: %s" % inFilePath2
+        print line2.rstrip()
+
+        inFile2.close()
+        inFile1.close()
+        sys.exit(1)
 
 inFile2.close()
 inFile1.close()
 
-if not areIdentical:
-    print "[FAILED] Files are not identical"
-    print "  ======================================"
-    print "  %s" % inFilePath1
-    for line in file(inFilePath1):
-        print "  %s" % line
-
-    print "  ======================================"
-
-    print "  %s" % inFilePath2
-    for line in file(inFilePath2):
-        print "  %s" % line
-    print "  ======================================"
-
+#if not areIdentical:
+#    print "[FAILED] Files are not identical:"
+#    print "  %s" % inFilePath1
+#    print "  %s" % inFilePath2
+#    print "  ======================================"
+#
+#
 #    inFile1 = open(inFilePath1)
 #    inFile2 = open(inFilePath2)
 #
@@ -48,5 +46,6 @@ if not areIdentical:
 #
 #    inFile1.close()
 #    inFile2.close()
-
-    sys.exit(1)
+#    print "  ======================================"
+#
+#    sys.exit(1)
