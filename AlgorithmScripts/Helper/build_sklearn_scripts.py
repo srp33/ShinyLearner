7a9ae3d7876efbf98cad2391ee6221b9da5620b8
Stephen Piccolo <goldfish1434@yahoo.com>
2017-06-24 23:04:11 -0600

    Simplified the way sklearn_f algorithms are specified when building those algorithm scripts.

--- AlgorithmScripts/Helper/build_sklearn_scripts.py
+++ AlgorithmScripts/Helper/build_sklearn_scripts.py
@@ -1,6 +1,6 @@
 import os, sys, shutil
 
-def createScript(algorithmType, templateFilePath, shortAlgName, algFileName, algInstantiation):
+def createScript(algorithmType, templateFilePath, shortAlgName, algFileName, algClass, algInstantiation):
     scriptDir = "../{}/tsv/sklearn/{}".format(algorithmType, shortAlgName)
     if not os.path.exists(scriptDir):
         os.makedirs(scriptDir, exist_ok=True)
@@ -13,78 +13,69 @@ def createScript(algorithmType, templateFilePath, shortAlgName, algFileName, alg
 
     with open(destFilePath) as templateFile:
         template = templateFile.read()
+
+        if algClass != None:
+            template = template.replace("{algorithmClass}", algClass)
+
         template = template.replace("{algorithmInstantiation}", algInstantiation)
 
     with open(destFilePath, 'w') as destFile:
         destFile.write(template)
 
-createScript("Classification", "sklearn_c_template", "adaboost", "default",
+createScript("Classification", "sklearn_c_template", "adaboost", "default", None,
     "from sklearn.ensemble import AdaBoostClassifier; " +
     "clf = AdaBoostClassifier(n_estimators=50, random_state=R_SEED)")
-createScript("Classification", "sklearn_c_template", "bagging", "default",
+createScript("Classification", "sklearn_c_template", "bagging", "default", None,
     "from sklearn.ensemble import BaggingClassifier; " +
     "clf = BaggingClassifier(n_estimators=50, random_state=R_SEED)")
-createScript("Classification", "sklearn_c_template", "decision_tree", "default",
+createScript("Classification", "sklearn_c_template", "decision_tree", "default", None,
     "from sklearn.tree import DecisionTreeClassifier; " +
     "clf = DecisionTreeClassifier(random_state=R_SEED)")
-createScript("Classification", "sklearn_c_template", "extra_trees", "default",
+createScript("Classification", "sklearn_c_template", "extra_trees", "default", None,
     "from sklearn.ensemble import ExtraTreesClassifier; " +
     "clf = ExtraTreesClassifier(n_estimators=50, random_state=R_SEED)")
-createScript("Classification", "sklearn_c_template", "gradient_boosting", "default",
+createScript("Classification", "sklearn_c_template", "gradient_boosting", "default", None,
     "from sklearn.ensemble import GradientBoostingClassifier; " +
     "clf = GradientBoostingClassifier(n_estimators=50, random_state=R_SEED)")
-createScript("Classification", "sklearn_c_template", "knn", "default",
+createScript("Classification", "sklearn_c_template", "knn", "default", None,
     "from sklearn.neighbors import KNeighborsClassifier; "
     "clf = KNeighborsClassifier()")
-createScript("Classification", "sklearn_c_template", "lda", "default",
+createScript("Classification", "sklearn_c_template", "lda", "default", None,
     "from sklearn.discriminant_analysis import LinearDiscriminantAnalysis; " +
     "clf = LinearDiscriminantAnalysis()")
-createScript("Classification", "sklearn_c_template", "logistic_regression", "default",
+createScript("Classification", "sklearn_c_template", "logistic_regression", "default", None,
     "from sklearn.linear_model import LogisticRegression; " +
     "clf = LogisticRegression(random_state=R_SEED)")
-createScript("Classification", "sklearn_c_template", "multilayer_perceptron", "default",
+createScript("Classification", "sklearn_c_template", "multilayer_perceptron", "default", None,
     "from sklearn.neural_network import MLPClassifier; "
     "clf = MLPClassifier(random_state=R_SEED)")
-createScript("Classification", "sklearn_c_template", "random_forest", "default",
+createScript("Classification", "sklearn_c_template", "random_forest", "default", None,
     "from sklearn.ensemble import RandomForestClassifier; " +
     "clf = RandomForestClassifier(n_estimators=50, random_state=R_SEED)")
-createScript("Classification", "sklearn_c_template", "sgd", "default",
+createScript("Classification", "sklearn_c_template", "sgd", "default", None,
     "from sklearn.linear_model import SGDClassifier; " +
     "clf = SGDClassifier(random_state=R_SEED, loss='modified_huber')") # It is necessary to use this loss function with sgd to produce probabilistic predictions
-createScript("Classification", "sklearn_c_template", "svm", "default",
+createScript("Classification", "sklearn_c_template", "svm", "default", None,
     "from sklearn.svm import SVC; " +
     "clf = SVC(probability=True, random_state=R_SEED)")
 
-createScript("FeatureSelection", "sklearn_f_template", "anova", "default",
+createScript("FeatureSelection", "sklearn_f_template", "anova", "default", "score",
     "from sklearn.feature_selection import f_classif; " +
-    "F, pval = f_classif(train_X, train_y); " +
-    "random_array = random.random(len(pval)); " +
-    "order = lexsort((random_array,pval)); " +
-    "rankedFeatures = [features[i] for i in order]")
-createScript("FeatureSelection", "sklearn_f_template", "mutual_info", "default",
+    "F, score = f_classif(train_X, train_y)")
+createScript("FeatureSelection", "sklearn_f_template", "mutual_info", "default", "score",
     "from sklearn.feature_selection import mutual_info_classif; " +
-    "pval = 1 - mutual_info_classif(train_X, train_y); " +
-    "random_array = random.random(len(pval)); " +
-    "order = lexsort((random_array,pval)); " +
-    "rankedFeatures = [features[i] for i in order]")
-createScript("FeatureSelection", "sklearn_f_template", "random_forest_rfe", "default",
+    "score = 1 - mutual_info_classif(train_X, train_y)")
+createScript("FeatureSelection", "sklearn_f_template", "random_forest_rfe", "default", "rfe",
     "from sklearn.ensemble import RandomForestClassifier; " +
     "from sklearn.feature_selection import RFE; " +
-    "estimator = RandomForestClassifier(n_estimators=50, random_state=R_SEED); " +
-    "selector = RFE(estimator, n_features_to_select=5, step=0.1); " +
-    "selector.fit(train_X, train_y); " +
-    "rankedFeatures = [y[1] for y in sorted(zip(map(lambda x: round(x, 4), selector.ranking_), features))]")
-createScript("FeatureSelection", "sklearn_f_template", "random_logistic_regression", "default",
+    "estimator = RandomForestClassifier(n_estimators=50, random_state=R_SEED)")
+createScript("FeatureSelection", "sklearn_f_template", "random_logistic_regression", "default", "coef",
     "from sklearn.linear_model import RandomizedLogisticRegression; " +
-    "scorer = RandomizedLogisticRegression(random_state=R_SEED); " +
-    "scorer.fit(train_X, train_y); " +
-    "rankedFeatures = [y[1] for y in sorted(zip(map(lambda x: round(x, 4), scorer.scores_), features), reverse=True)]")
-createScript("FeatureSelection", "sklearn_f_template", "svm_rfe", "default",
+    "scorer = RandomizedLogisticRegression(random_state=R_SEED)")
+createScript("FeatureSelection", "sklearn_f_template", "svm_rfe", "default", "rfe",
     "from sklearn.svm import SVC; " +
     "from sklearn.feature_selection import RFE; " +
-    "estimator = SVC(random_state=R_SEED, kernel='linear'); " +
-    "selector = RFE(estimator, n_features_to_select=5, step=0.1); " +
-    "selector.fit(train_X, train_y); rankedFeatures = [y[1] for y in sorted(zip(map(lambda x: round(x, 4), selector.ranking_), features))]")
+    "estimator = SVC(random_state=R_SEED, kernel='linear')")
 
 ## Failed (classification): gaussian_naivebayes gaussian_process qda
 ## Failed (feature selection): random_lasso
--- AlgorithmScripts/Helper/sklearn_f_generic.py
+++ AlgorithmScripts/Helper/sklearn_f_generic.py
@@ -1,3 +1,5 @@
+# See http://blog.datadive.net/selecting-good-features-part-iv-stability-selection-rfe-and-everything-side-by-side/
+
 from sys import argv
 from pandas import read_csv
 from numpy import float32
@@ -6,56 +8,8 @@ from numpy import lexsort
 from numpy import random
 
 dataFilePath = argv[1]
-algorithmInstantiation = argv[2]
-
-# See http://blog.datadive.net/selecting-good-features-part-iv-stability-selection-rfe-and-everything-side-by-side/
-
-def rank_features(algorithm, X, y):
-    if algorithm == 'anova':
-        from sklearn.feature_selection import f_classif
-        F, pval = f_classif(X, y)
-        random_array = random.random(len(pval))
-        order = lexsort((random_array,pval)) # will break ties by random
-
-        return [features[i] for i in order]
-
-    elif algorithm == 'mutual_info':
-        from sklearn.feature_selection import mutual_info_classif
-        pval = 1 - mutual_info_classif(X, y)
-        random_array = random.random(len(pval))
-        order = lexsort((random_array,pval)) # will break ties by random
-
-        return [features[i] for i in order]
-
-    # The RFE approach can be used with various different classifiers
-    elif algorithm == 'random_forest_rfe':
-        from sklearn.ensemble import RandomForestClassifier
-        from sklearn.feature_selection import RFE
-        estimator = RandomForestClassifier(n_estimators=50, random_state=R_SEED)
-        selector = RFE(estimator, n_features_to_select=5, step=0.1)
-        selector.fit(X, y)
-
-        return [y[1] for y in sorted(zip(map(lambda x: round(x, 4), selector.ranking_), features))]
-
-    elif algorithm == 'random_logistic_regression':
-        from sklearn.linear_model import RandomizedLogisticRegression
-        scorer = RandomizedLogisticRegression(random_state=R_SEED)
-        scorer.fit(X, y)
-
-        return [y[1] for y in sorted(zip(map(lambda x: round(x, 4), scorer.scores_), features), reverse=True)]
-
-    elif algorithm == 'svm_rfe':
-        from sklearn.svm import SVC
-        from sklearn.feature_selection import RFE
-        estimator = SVC(random_state=R_SEED, kernel='linear')
-        selector = RFE(estimator, n_features_to_select=5, step=0.1)
-        selector.fit(X, y)
-
-        return [y[1] for y in sorted(zip(map(lambda x: round(x, 4), selector.ranking_), features))]
-
-    else:
-        print "Invalid algorithm: %s" % algorithm
-        exit(1)
+algorithmClass = argv[2]
+algorithmInstantiation = argv[3]
 
 R_SEED = 0
 random.seed(R_SEED)
@@ -69,8 +23,30 @@ train_y = array([classOptions.index(y) for y in train_y])
 
 features = list(train_df.columns.values)[:-1]
 
-# Dynamically perform feature selection
+# Dynamically execute code
 code = compile(algorithmInstantiation, "<string>", 'exec')
 exec code
+
+rankedFeatures = None
+
+if algorithmClass == 'score':
+    random_array = random.random(len(score))
+    order = lexsort((random_array, score)) # will break ties by random
+
+    rankedFeatures = [features[i] for i in order]
+
+# The RFE approach can be used with various different classifiers
+elif algorithmClass == 'rfe':
+    selector = RFE(estimator, n_features_to_select=5, step=0.1)
+    selector.fit(train_X, train_y)
+
+    rankedFeatures = [y[1] for y in sorted(zip(map(lambda x: round(x, 4), selector.ranking_), features))]
+
+elif algorithmClass == 'coef':
+    scorer.fit(train_X, train_y)
+    rankedFeatures = [y[1] for y in sorted(zip(map(lambda x: round(x, 4), scorer.scores_), features), reverse=True)]
+
+else:
+    print("Invalid algorithm type.")
 
 print ",".join(rankedFeatures)
