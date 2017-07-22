c6d3411a7994266c9bad2183b3b35d5a0e645afb
Stephen Piccolo <goldfish1434@yahoo.com>
2017-07-22 09:58:38 -0600

    Enabled support for algorithm optimization in nestedclassification and temporarily removed majority vote support in classification

--- scripts/SelectBestInnerResults_Classification.R
+++ scripts/SelectBestInnerResults_Classification.R
@@ -9,8 +9,7 @@ data <- read.table(inFilePath, sep="\t", header=TRUE, row.names=NULL, quote="\""
 data <- filter(data, Metric=="AUROC")
 data <- select(data, -Metric)
 
-###
-write.table(arrange(data, CL), "/Users/srp33/Downloads/aa.txt", sep="\t", quote=F, col.names=T, row.names=F)
+### write.table(arrange(data, CL), "/Users/srp33/Downloads/aa.txt", sep="\t", quote=F, col.names=T, row.names=F)
 
 # Average across the inner iterations
 data <- ungroup(summarise(group_by(data, Description, CL), Value=mean(Value)))
@@ -18,24 +17,25 @@ data <- ungroup(summarise(group_by(data, Description, CL), Value=mean(Value)))
 # Truncate parameter combos to directory names
 data$CL_Dir <- factor(dirname(as.character(data$CL)))
 
-###
-write.table(arrange(data, CL), "/Users/srp33/Downloads/bb.txt", sep="\t", quote=F, col.names=T, row.names=F)
+### write.table(arrange(data, CL), "/Users/srp33/Downloads/bb.txt", sep="\t", quote=F, col.names=T, row.names=F)
 
 # Pick best result for each description
 groupedData <- group_by(data, Description, CL_Dir)
 set.seed(0)
-###groupedData <- filter(groupedData, rank(-Value, ties.method="random")==1) %>% ungroup() %>% select(-Value, -CL_Dir)
-groupedData <- filter(groupedData, rank(-Value, ties.method="random")==1) %>% ungroup() %>% select(-CL_Dir)
+groupedData <- filter(groupedData, rank(-Value, ties.method="random")==1) %>% ungroup() %>% select(-Value)
 
-###
-write.table(arrange(groupedData, CL), "/Users/srp33/Downloads/cc.txt", sep="\t", quote=F, col.names=T, row.names=F)
+### write.table(arrange(groupedData, CL), "/Users/srp33/Downloads/cc.txt", sep="\t", quote=F, col.names=T, row.names=F)
 
 trainTestData <- read.table(trainTestFilePath, sep="\t", header=FALSE, row.names=NULL, quote="\"", check.names=F)
 colnames(trainTestData) <- c("Description", "TrainIDs", "TestIDs")
 
 mergedData <- inner_join(groupedData, trainTestData, by="Description")
 
-mergedData$Description <- paste(mergedData$Description, "____Ensemble_Select_Best", sep="")
+### write.table(arrange(mergedData, CL), "/Users/srp33/Downloads/dd.txt", sep="\t", quote=F, col.names=T, row.names=F)
 
-outCLData <- select(mergedData, Description, TrainIDs, TestIDs, CL)
-write.table(outCLData, outCLFilePath, sep="\t", col.names=F, row.names=F, quote=F)
+mergedData$Description <- paste(mergedData$Description, "____", mergedData$CL_Dir, sep="")
+#mergedData$Description <- paste(mergedData$Description, "____Ensemble_Select_Best", sep="")
+
+mergedData <- select(mergedData, Description, TrainIDs, TestIDs, CL)
+
+write.table(mergedData, outCLFilePath, sep="\t", col.names=F, row.names=F, quote=F)
