23eba3d61597133f0b66def4293e6f498f872a76
Stephen Piccolo <goldfish1434@yahoo.com>
2017-07-22 21:59:49 -0600

    Added code to debug specifying locations of algorithm scripts.

--- scripts/AddAlgorithmScripts.py
+++ scripts/AddAlgorithmScripts.py
@@ -3,17 +3,32 @@ import os, sys, glob
 trainTestFilePath = sys.argv[1]
 classifAlgosRaw = sys.argv[2]
 outFilePath = sys.argv[3]
+verbose = sys.argv[4] == "true"
+
+def smartPrint(output):
+    if verbose:
+        print(output)
+
+smartPrint("pwd:")
+smartPrint(os.path.dirname(os.path.realpath(__file__)))
 
 classifAlgos = set()
 for x in classifAlgosRaw.split(","):
+
+    smartPrint("x:" + x)
     for y in glob.glob(x):
+        smartPrint("y:" + y)
         if os.path.isdir(y):
+            smartPrint("isdir")
             for z in glob.glob(y.rstrip("/") + "/*"):
+                smartPrint("zz:" + z)
                 classifAlgos.add(z)
         elif y.endswith(".list"):
+            smartPrint("islist")
             for line in file(y):
                 classifAlgos.add(line.rstrip())
         else:
+            smartPrint("other")
             classifAlgos.add(y)
 
 classifAlgos = sorted(list(classifAlgos))
