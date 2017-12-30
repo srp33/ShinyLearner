7c2a1c19f508cb3594a0ce681f083ff6fa323f47
Stephen Piccolo <goldfish1434@yahoo.com>
2017-12-29 22:45:10 -0700

    Debugging parallelization

--- AlgorithmScripts/Helper/sklearn_c_generic.py
+++ AlgorithmScripts/Helper/sklearn_c_generic.py
@@ -1,58 +1,63 @@
-from pandas import read_csv
-from numpy import array
-from sys import argv
-from sys import exit
-from numpy import random
+import multiprocessing
 
-from sklearn.ensemble import AdaBoostClassifier
-from sklearn.ensemble import BaggingClassifier
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.ensemble import ExtraTreesClassifier
-from sklearn.ensemble import GradientBoostingClassifier
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
-from sklearn.linear_model import LogisticRegression
-from sklearn.neural_network import MLPClassifier
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.linear_model import SGDClassifier
-from sklearn.svm import SVC
+if __name__ == '__main__':
+    multiprocessing.set_start_method('spawn')
 
-trainFile = argv[1]
-testFile = argv[2]
-classOptions = argv[3].split(",")
-numCores = argv[4]
-algorithmInstantiation = argv[5]
+    from pandas import read_csv
+    from numpy import array
+    from sys import argv
+    from sys import exit
+    from numpy import random
 
-def readData(inFilePath):
-    return read_csv(inFilePath, sep='\t', index_col=0)
+    from sklearn.ensemble import AdaBoostClassifier
+    from sklearn.ensemble import BaggingClassifier
+    from sklearn.tree import DecisionTreeClassifier
+    from sklearn.ensemble import ExtraTreesClassifier
+    from sklearn.ensemble import GradientBoostingClassifier
+    from sklearn.neighbors import KNeighborsClassifier
+    from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+    from sklearn.linear_model import LogisticRegression
+    from sklearn.neural_network import MLPClassifier
+    from sklearn.ensemble import RandomForestClassifier
+    from sklearn.linear_model import SGDClassifier
+    from sklearn.svm import SVC
 
-R_SEED = 0
-random.seed(R_SEED)
+    trainFile = argv[1]
+    testFile = argv[2]
+    classOptions = argv[3].split(",")
+    numCores = argv[4]
+    algorithmInstantiation = argv[5]
 
-train_df = readData(trainFile)
-train_X = train_df.ix[:,:-1].values
-train_y = array([classOptions.index(str(y[0])) for y in train_df.loc[:,["Class"]].values.tolist()])
+    def readData(inFilePath):
+        return read_csv(inFilePath, sep='\t', index_col=0)
 
-test_X = readData(testFile).values
+    R_SEED = 0
+    random.seed(R_SEED)
 
-algorithmInstantiation = algorithmInstantiation.replace("{n_jobs}", numCores)
+    train_df = readData(trainFile)
+    train_X = train_df.ix[:,:-1].values
+    train_y = array([classOptions.index(str(y[0])) for y in train_df.loc[:,["Class"]].values.tolist()])
+
+    test_X = readData(testFile).values
+
+    algorithmInstantiation = algorithmInstantiation.replace("{n_jobs}", numCores)
 
 # Dynamically create the algorithm object
-code = compile(algorithmInstantiation, "<string>", 'exec')
-exec(code)
+    code = compile(algorithmInstantiation, "<string>", 'exec')
+    exec(code)
 
-clf.fit(train_X, train_y)
-probs = clf.predict_proba(test_X)
+    clf.fit(train_X, train_y)
+    probs = clf.predict_proba(test_X)
 
-for i in range(len(probs)):
-    iProbs = list(probs[i])
+    for i in range(len(probs)):
+        iProbs = list(probs[i])
 
-    maxProb = max(iProbs)
-    indicesMatchingMax = [i for i in range(len(iProbs)) if iProbs[i]==maxProb]
-    random.shuffle(indicesMatchingMax)
+        maxProb = max(iProbs)
+        indicesMatchingMax = [i for i in range(len(iProbs)) if iProbs[i]==maxProb]
+        random.shuffle(indicesMatchingMax)
 
-    prediction = classOptions[indicesMatchingMax[0]]
+        prediction = classOptions[indicesMatchingMax[0]]
 
-    print("{}\t{}".format(prediction, "\t".join(["{:.9f}".format(iProb) for iProb in iProbs])))
+        print("{}\t{}".format(prediction, "\t".join(["{:.9f}".format(iProb) for iProb in iProbs])))
 
-exit(0)
+    exit(0)
