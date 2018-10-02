df017ebafcbb9241f007baec2b225836031a51f9
Kimball Hill <hillkimball@gmail.com>
2018-10-02 10:53:24 -0600

    added snn and additional hyperparameter options

--- AlgorithmScripts/Classification/tsv/keras/dnn/dnn.py
+++ AlgorithmScripts/Classification/tsv/keras/dnn/dnn.py
@@ -1,26 +1,30 @@
 from sys import argv
+import os
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.metrics import roc_auc_score
 from tensorflow.keras.models import Model
-from tensorflow.keras.layers import Dropout, Dense, Input, Flatten, BatchNormalization, Activation
+from tensorflow.keras.layers import Dropout, Dense, Input, Flatten, BatchNormalization, Activation, AlphaDropout
 from tensorflow.keras.regularizers import l2
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.losses import binary_crossentropy, categorical_crossentropy
-from tensorflow.keras import metrics
 from tensorflow.keras.utils import multi_gpu_model
 import tensorflow as tf
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 TRAIN_FILE = argv[1]
 TEST_FILE = argv[2]
 CLASS_OPTIONS = argv[3].split(',')
 NUM_CORES = argv[4]
 VERBOSE = argv[5] == 'true'
-DROPOUT_RATE = float(argv[6])
-REGULARIZATION_RATE = float(argv[7])
-BATCH_NORMALIZATION = argv[8] == 'true'
-EPOCHS = int(argv[9])
+MODEL_TYPE = argv[6]
+LAYERS = argv[7].split(',')
+DROPOUT_RATE = float(argv[8])
+REGULARIZATION_RATE = float(argv[9])
+BATCH_NORMALIZATION = (MODEL_TYPE != 'snn' and argv[10] == 'true')
+EPOCHS = int(argv[11])
 
 train_df = pd.read_csv(TRAIN_FILE, sep='\t', index_col=0)
 x_train = train_df.drop('Class', axis=1).values
@@ -36,38 +40,39 @@ def auroc(y_true, y_pred):
 
 
 def dnn(x, y, test):
-    layers = [16, 16]
+    activation = 'selu' if MODEL_TYPE == 'snn' else 'elu'
+    dropout = AlphaDropout if MODEL_TYPE == 'snn' else Dropout
+    layers = []
+    for layer in LAYERS:
+        if int(layer) > 0:
+            layers.append(int(layer))
     n_inputs = x.shape[1]
     n_outputs = y.shape[1]
     input_layer = Input(shape=(n_inputs,), name='input')
     layer = Flatten()(input_layer)
     if DROPOUT_RATE > 0:
-        layer = Dropout(DROPOUT_RATE, name='dropout_input')(layer)
+        layer = dropout(DROPOUT_RATE, name='dropout_input')(layer)
     for i in range(len(layers)):
         layer = Dense(layers[i],
                       kernel_regularizer=l2(REGULARIZATION_RATE),
-                      activation='elu',
+                      activation=activation,
                       name='dense_{}'.format(i + 1))(layer)
         if BATCH_NORMALIZATION:
             layer = BatchNormalization(name='batch_norm_{}'.format(i + 1))(layer)
         if DROPOUT_RATE > 0:
-            layer = Dropout(DROPOUT_RATE, name='dropout_{}'.format(i + 1))(layer)
+            layer = dropout(DROPOUT_RATE, name='dropout_{}'.format(i + 1))(layer)
     logits = Dense(n_outputs, name='logits')(layer)
     probabilities = Activation('softmax', name='output')(logits)
     if n_outputs == 2:
         loss = binary_crossentropy
-        metric = metrics.binary_crossentropy
     else:
         loss = categorical_crossentropy
-        metric = metrics.categorical_crossentropy
     model = Model(input_layer, probabilities)
     try:
         model = multi_gpu_model(model, gpus=2)
     except:
-        # print('Problem using multiple GPUs...', flush=True)
         pass
-    model.compile(optimizer=Adam(), loss=loss, metrics=['accuracy', metric, auroc])
-
+    model.compile(optimizer=Adam(), loss=loss, metrics=['accuracy', auroc])
     model.fit(x, y, verbose=VERBOSE, epochs=EPOCHS)
     predictions = model.predict(test, verbose=VERBOSE)
     for prediction in predictions:
