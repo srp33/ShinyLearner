81ce0aacc003b017e0d9bc7becbf8b486f2366fc
Stephen Piccolo <stephen.piccolo.byu@gmail.com>
2019-01-14 16:43:02 -0700

    Added various (though not yet comprehensive) parameter options for the keras algorithms

--- AlgorithmScripts/Helper/build_keras_scripts.py
+++ AlgorithmScripts/Helper/build_keras_scripts.py
@@ -16,13 +16,13 @@ packagePath = "tsv/keras"
 #############################################################
 
 dnn = "{model_type};{layers};{dropout};{regularization};{batch_norm};{epochs}"
-createScripts("Classification", packagePath, "keras_c_generic_template", "dnn", None, dnn, {"model_type": ["dnn"], "layers": ["16,16"], "dropout": ["0.5"], "regularization": ["0.1"], "batch_norm": ["true"], "epochs": ["100"]}, summaryDict)
+createScripts("Classification", packagePath, "keras_c_generic_template", "dnn", None, dnn, {"model_type": ["dnn"], "layers": ["16,16", "16,16,16,16", "300,100"], "dropout": ["0.1", "0.5"], "regularization": ["0.1", "0.2", "0.01"], "batch_norm": ["true"], "epochs": ["100", "10", "50"]}, summaryDict)
 
 snn = "{model_type};{layers};{dropout};{regularization};{batch_norm};{epochs}"
-createScripts("Classification", packagePath, "keras_c_generic_template", "snn", None, snn, {"model_type": ["snn"], "layers": ["16,16"], "dropout": ["0.2"], "regularization": ["0.1"], "batch_norm": ["false"], "epochs": ["100"]}, summaryDict)
+createScripts("Classification", packagePath, "keras_c_generic_template", "snn", None, snn, {"model_type": ["snn"], "layers": ["16,16", "32,32,32,32", "300,100"], "dropout": ["0.2", "0.5"], "regularization": ["0.1", "0.2", "0.01"], "batch_norm": ["true"], "epochs": ["100", "10", "50"]}, summaryDict)
 
 resnet = "{layer_width};{layers};{dropout};{regularization};{activation};{learning_rate};{epochs}"
-createScripts("Classification", packagePath, "keras_c_resnet_template", "resnet", None, resnet, {"layer_width": ["16"], "layers": ["50"], "dropout": ["0.5"], "regularization": ["0.01"], "activation": ["selu"], "learning_rate": ["0.001"], "epochs": ["500"]}, summaryDict)
+createScripts("Classification", packagePath, "keras_c_resnet_template", "resnet", None, resnet, {"layer_width": ["16", "32"], "layers": ["50", "100"], "dropout": ["0.5"], "regularization": ["0.1", "0.01", "0.001"], "activation": ["selu"], "learning_rate": ["0.001"], "epochs": ["10", "100", "500"]}, summaryDict)
 
 if showStats:
     print("#######################################")
