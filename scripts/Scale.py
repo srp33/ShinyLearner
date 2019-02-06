8bdb9a037fd80e08c7bfdf65c3b478a012941f1f
Kimball Hill <hillkimball@gmail.com>
2019-02-06 12:17:14 -0700

    detect and remove non-numerical and categorical columns before scaling

--- scripts/Scale.py
+++ scripts/Scale.py
@@ -24,41 +24,68 @@ scaler = argv[9]
 
 def scale_data(file_path, scaler='robust', verbose=False):
     scaler = scaler.lower()
-    gzip = False
-    if file_path.endswith('.gz'):
-        gzip = True
     data = pd.read_csv(file_path, sep='\t', index_col=0)
     index = data.index
-    x = data.drop('Class', axis=1)
-    x_columns = x.columns
-    y = data[['Class']]
-    if scaler == 'standard':
-        transformer = StandardScaler()
-    elif scaler == 'minmax':
-        transformer = MinMaxScaler()
-    elif scaler == 'maxabs':
-        transformer = MaxAbsScaler()
-    elif 'power' in scaler:
-        transformer = PowerTransformer()
-    elif 'quantile' in scaler:
-        if 'normal' in scaler:
-            if verbose:
-                print('Outputting normal (Gaussian) distribution')
-            transformer = QuantileTransformer(output_distribution='normal')
-        else:
-            transformer = QuantileTransformer(output_distribution='uniform')
-            if verbose:
-                print('Outputting uniform distribution')
-    elif scaler == 'normalizer':
-        transformer = Normalizer()
-    else:
-        transformer = RobustScaler()
+
+    # Remove columns that don't contain numerical values
+    non_num_columns = ['Class']
+    dtypes = data.columns.to_series().groupby(data.dtypes).groups
+    dtypes = {k.name: list(v) for k, v in dtypes.items()}
+    for dtype in dtypes.keys():
+        if 'float' not in dtype and 'int' not in dtype:
+            non_num_columns += dtypes[dtype]
     if verbose:
-        print('Using scaler of class "{}"'.format(str(type(transformer)).split("'")[1]))
-    x = transformer.fit_transform(x)
-    x = pd.DataFrame(x, index=index, columns=x_columns)
-    data = pd.concat([x, y], axis=1, join='inner')
-    data.to_csv(file_path, index_label='', sep='\t', compression='gzip' if gzip else None)
+        print('The following columns do not contain numerical values and will not be scaled:')
+        print('\t', ', '.join(set(non_num_columns)))
+
+    # Remove columns that contain numbers, but fewer than half are distinct values
+    categorical_columns = []
+    num_unique = data.columns.to_series().groupby(data.nunique()).groups
+    num_unique = {k: list(v) for k, v in num_unique.items()}
+    for num in num_unique.keys():
+        if num < len(index) * 0.5:
+            categorical_columns += num_unique[num]
+    if verbose and len(categorical_columns) > 0:
+        print('In the following columns, fewer than half are distinct values, so they will not be scaled:')
+        print('\t', ', '.join(categorical_columns))
+
+    excluded_columns = set(non_num_columns + categorical_columns)
+    excluded_data = data[excluded_columns]
+    scaled_data = data.drop(excluded_columns, axis=1)
+    scaled_columns = scaled_data.columns
+    if len(scaled_columns) > 0:
+        if scaler == 'standard':
+            transformer = StandardScaler()
+        elif scaler == 'minmax':
+            transformer = MinMaxScaler()
+        elif scaler == 'maxabs':
+            transformer = MaxAbsScaler()
+        elif 'power' in scaler:
+            transformer = PowerTransformer()
+        elif 'quantile' in scaler:
+            if 'normal' in scaler:
+                if verbose:
+                    print('Outputting normal (Gaussian) distribution')
+                transformer = QuantileTransformer(output_distribution='normal')
+            else:
+                transformer = QuantileTransformer(output_distribution='uniform')
+                if verbose:
+                    print('Outputting uniform distribution')
+        elif scaler == 'normalizer':
+            transformer = Normalizer()
+        else:
+            transformer = RobustScaler()
+        if verbose:
+            print('Using scaler of class "{}" to scale data'.format(str(type(transformer)).split("'")[1]))
+        scaled_data = transformer.fit_transform(scaled_data)
+        scaled_data = pd.DataFrame(scaled_data, index=index, columns=scaled_columns)
+        data = pd.concat([scaled_data, excluded_data], axis=1, join='inner')
+        if verbose:
+            print('Saving scaled version of data to {}'.format(file_path))
+        data.to_csv(file_path, index_label='', sep='\t')
+    else:
+        if verbose:
+            print('No columns remain to be scaled, no action was performed')
 
 
 scale_data(file_path, scaler, verbose)
