5528f84b0976508ac0ed50c45961316f077fb69f
Stephen Piccolo <stephen.piccolo.byu@gmail.com>
2019-04-29 13:27:19 -0600

    No longer creating output directory (to avoid permissions problems)

--- WebApp/server.R
+++ WebApp/server.R
@@ -9,7 +9,7 @@ library(shiny)
 shinyServer(function(input, output, session) {
   
   session$allowReconnect(TRUE)
-  docker_image_name <- 'srp33/shinylearner:version486'
+  docker_image_name <- 'srp33/shinylearner:version487'
   numFeaturesOptions <- '1,10,100,1000,10000'
   defaultValidation <- 'mc'
   validationChoices <- list('Monte Carlo cross validation' = 'mc', 'k-fold cross validation' = 'kf')
